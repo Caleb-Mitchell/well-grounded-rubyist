6c338b3c1f5584af98990d791d653c4bff8a9c73
Caleb-Mitchell <63262011+Caleb-Mitchell@users.noreply.github.com>
2023-09-05 17:46:41 -0500

    Continue collection chapter notes

--- chapter_9_collection_and_container_objects/notes.rb
+++ chapter_9_collection_and_container_objects/notes.rb
@@ -11,9 +11,14 @@
 
 # using `each_with_index`
 # hash = { red: "ruby", white: "diamond", green: "emerald" }
-# hash.each_with_index do |(key, value), i|
-#   puts "Pair #{i} is: #{key}/#{value}"
+# hash.each_with_index do |(key, value), idx|
+#   puts "Pair #{idx} is: #{key}/#{value}"
 # end
+
+# arr = [1, 2]
+# (one, two) = arr
+# p one
+# p two
 
 # interesting syntax in book with curly braces, solargraph doesn't like it
 # hash.each_with_index { |(key, value), idx| p "Pair #{idx} is: #{key}/#{value}" }
@@ -33,10 +38,12 @@
 # new_hash = array.each_slice(2).to_h
 # p new_hash
 
-# array = [[:name, "Joe"], [:age, 10], [:favorite_color, "blue"]]
+# array = [[:name, "Joe", 30], [:age, 10], [:favorite_color, "blue"]]
 # p array.to_h
 
 # 9.2.1
+
+# p([1, 2, 3].interest([3, 4, 5]))
 
 # a = Array.new
 # p a
@@ -46,7 +53,7 @@
 
 # a = Array.new(3, "abc")
 # p a
-# a[0] << "def"
+# a[0] += "def"
 # p a
 # puts a[1]
 # p Array.new(3) { "abc" }
@@ -62,7 +69,7 @@
 #   puts num if num.match?(/[13]/)
 # end
 
-# a = %q["It was the 'best' of times"]
+# a = %q("It was the 'best' of times")
 # p a
 
 # call an anonymous function with square brackets, chatgpt example
@@ -83,34 +90,116 @@ websitehttps://www.rubyguides.com/2016/02/ruby-procs-and-lambdas/. I hope this
 helps you understand how square brackets work in Ruby. blush
 =end
 
+# p self.class
 # string = "A string"
-# string.respond_to?(:to_ary) # => false
-# string.respond_to?(:to_a) # => false
-# Array(string) # => ["A string"]
+# p string.respond_to?(:to_ary) # => false
+# p string.respond_to?(:to_a) # => false
+# p Array(string) # => ["A string"]
+# p arr = [string]
+# p arr = Array.new(1, "A string")
+
 # def string.to_a
-#   split(//)
+#   chars
 # end
 # Array(string) # => ["A", " ", "s", "t", "r", "i", "n", "g"]
 
-# %w(Joe Leo III) # => ["Joe", "Leo", "III"]
+# p %w(Joe Leo III) # => ["Joe", "Leo", "III"]
 # %w{ Joe Leo III } # => ["Joe", "Leo", "III"]
-# %w(David\ A.\ Black is a Rubyist.) # => ["David A. Black", "is", "a", "Rubyist."]
+# p %w(David\ A.\ Black is a Rubyist.) # => ["David A. Black", "is", "a", "Rubyist."]
 # what's the difference with below?
-# %W(Joe is #{2018 - 1981} years old.) # => ["Joe", "is", "37", "years", "old."]
+# p %W(Joe is #{2018 - 1981} years old.) # => ["Joe", "is", "37", "years", "old."]
 
-# %i(a b c) # => [:a, :b, :c]
+# p %i(a b c) # => [:a, :b, :c]
 # d = "David"
+# p %i('#{d}') # => [:"\"David\""]
 # p %I("#{d}") # => [:"\"David\""]
 
 # obj = Object.new
-# Array.try_convert(obj) # => nil
+# p Array.try_convert(obj) # => nil
 # def obj.to_ary
 #   [1, 2, 3]
 # end # => :to_ary
-# Array.try_convert(obj) # => [1, 2, 3]
-# def obj.to_a
+# p Array.try_convert(obj) # => [1, 2, 3]
+# def obj.to_ary
 #   "Not an array!"
 # end # => :to_a
 # Array.try_convert(obj) # => TypeError: can't convert Object to Array (Object#to_ary gives String...
 
 # 9.2.2
+
+# a = []
+# a[0] = "first"
+# # a.[]=(1, "first")
+# p a
+
+# a = [1, 2, 3, 4, 5]
+# p a[2]
+
+# a = %w(red orange yellow purple gray indigo violet) # => ["red", "orange", "yellow", "purple", "gray", "indigo", "violet"]
+# p a[3, 2] # => ["purple", "gray"]
+# a[3, 2] = "green", "blue" # => ["green", "blue"]
+# p a # => ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+# a = %w(red orange yellow purple gray indigo violet)
+# p a # => ["red", "orange", "yellow", "purple", "gray", "indigo", "violet"]
+# p a[3..5] # => ["purple", "gray", "indigo"]
+# a[1..2] = "green", "blue" # => ["green", "blue"]
+# p a # => ["red", "green", "blue", "purple", "gray", "indigo", "violet"]
+
+# array = %w(the dog ate the cat)
+# articles = array.values_at(0, 3, 4, 5)
+# p articles # => ["the", "the"]
+
+# arr = [[1], 2, 3, [4, 5]]
+# p arr[0] # => [1]
+# p arr[3][0] # => 4
+# p arr.dig(3, 0) # => 4
+
+# p [["Joe", %w(loves Lucy,), "his"], "adorable", ["daughter"]].fetch(6) # => "Lucy,"
+
+# a = [1, 2, 3, 4]
+# a.unshift(0)
+# p a # => [0, 1, 2, 3, 4]
+
+# a = [1, 2, 3, 4]
+# a << 5 << 6
+# p a # => [1, 2, 3, 4, 5]
+
+# a = [1, 2, 3, 4]
+# a.append(5)
+# p a # => [1, 2, 3, 4, 5]
+
+# a = [1, 2, 3, 4, 5]
+# p a.pop # => 5
+# p a # => [1, 2, 3, 4]
+# p a.shift # => 1
+# p a # => [2, 3, 4]
+
+a = %w(one two three four five)
+p a.pop(2) # => ["four", "five"]
+# p a # => ["one", "two", "three"]
+p a.shift(1) # => ["one", "two"]
+# p a # => ["three"]
+
+# 9.2.3
+
+# p [1, 2, 3].concat([4, 5, 6]) # => [1, 2, 3, 4, 5, 6]
+# p [1, 2, 3].push([4, 5, 6]) # => [1, 2, 3, [4, 5, 6]]
+
+# a = [1,2,3]
+# b = a + [4,5,6]
+# p a # => [1, 2, 3]
+
+# a = [1,2,3]
+# a.replace([4,5,6])
+# p a # => [4, 5, 6]
+
+# a = [1, 2, 3]
+# p a.object_id # => 60
+# a = [4, 5, 6]
+# p a.object_id # => 80
+
+# a = [1, 2, 3]
+# p a.object_id # => 60
+# b = a
+# p b.object_id # => 60
